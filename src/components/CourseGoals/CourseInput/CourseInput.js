e1d3794354861f1db4e69ca232c64a272eb86668
Alan Lovell <git@nlovell.net>
2022-07-02 16:03:07 +0100

    dynamic css through template literals

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,19 +5,31 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    inputValidation();
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    inputValidation();
     props.onAddGoal(enteredValue);
   };
 
+  const inputValidation = () => {
+    if(enteredValue.trim().length === 0){
+      setIsValid(false);
+      return;
+    }
+    setIsValid(true);
+      return; 
+  }
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${isValid ? '' : 'invalid'}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
